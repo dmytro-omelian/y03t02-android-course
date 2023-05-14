5981f576d020b6e7af16eca8907d5211430ff1a3
Dichik <omeluan.dima@gmail.com>
2023-05-14 19:10:58 +0300

    assignment 2: update and add ContentFragment

--- app/src/main/java/com/example/myapplication/ContentFragment.java
+++ app/src/main/java/com/example/myapplication/ContentFragment.java
@@ -1,48 +1,45 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import org.jetbrains.annotations.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ContentFragment extends Fragment {
 
+    private Fragment outputFragment;
     public ContentFragment() {
-        super(R.layout.activity_main);
-    }
-
-    private String getResponseFromCheckBox(boolean answerIsYes, boolean answerIsNo) {
-        if (!answerIsYes && !answerIsNo) {
-            return "No Answer";
-        }
-        if (answerIsYes) {
-            return "Yes";
-        }
-        return "No";
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
         Button button = view.findViewById(R.id.button);
-        EditText input = view.findViewById(R.id.textInputEditText);
-        RadioButton radioYes = view.findViewById(R.id.radio_yes);
-        RadioButton radioNo = view.findViewById(R.id.radio_no);
-        TextView output = view.findViewById(R.id.output);
-
         button.setOnClickListener(view1 -> {
-            String message = String.format("Question: %s \nAnswer: %s", input.getText(),
-                    getResponseFromCheckBox(radioYes.isChecked(), radioNo.isChecked()));
-            output.setText(message);
+            TextInputEditText inputEditText = view.findViewById(R.id.textInputEditText);
+            String message = (inputEditText.getText() == null) ? null : inputEditText.getText().toString();
+
+            outputFragment = OutputFragment.newInstance(getOutputMessage(message));
+
+            assert getFragmentManager() != null;
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment, outputFragment, null);
+            transaction.commit();
         });
+        return view;
+    }
+
+    private String getOutputMessage(String message) {
+        return (message == null) ? "Hello from OutputFragment!" : message;
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/ContentFragment2.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.Nullable;
+
+public class ContentFragment2 extends Fragment {
+
+    public ContentFragment2() {
+        super(R.layout.activity_main);
+    }
+
+    private String getResponseFromCheckBox(boolean answerIsYes, boolean answerIsNo) {
+        if (!answerIsYes && !answerIsNo) {
+            return "No Answer";
+        }
+        if (answerIsYes) {
+            return "Yes";
+        }
+        return "No";
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        Button button = view.findViewById(R.id.button);
+//        EditText input = view.findViewById(R.id.textInputEditText);
+//        RadioButton radioYes = view.findViewById(R.id.radio_yes);
+//        RadioButton radioNo = view.findViewById(R.id.radio_no);
+//        TextView output = view.findViewById(R.id.output);
+//
+//        button.setOnClickListener(view1 -> {
+//            String message = String.format("Question: %s \nAnswer: %s", input.getText(),
+//                    getResponseFromCheckBox(radioYes.isChecked(), radioNo.isChecked()));
+//            output.setText(message);
+//        });
+    }
+
+}
