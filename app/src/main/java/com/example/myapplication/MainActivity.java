b8b332d60dcadeaa2151fdf01ceebcef3e958492
Dichik <omeluan.dima@gmail.com>
2023-05-14 19:10:03 +0300

    assignment 2: update MainActivity component

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container_view, ContentFragment.class, null)
-                    .commit();
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
