ac30e31a1d73c2344ef1ca39fae9603583ed2364
Dichik <omeluan.dima@gmail.com>
2023-05-15 10:17:28 +0300

    assignment 2: update ContentFragment with ability to communicate with MainActivity

--- app/src/main/java/com/example/myapplication/ContentFragment.java
+++ app/src/main/java/com/example/myapplication/ContentFragment.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,36 +12,73 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class ContentFragment extends Fragment {
 
-    private Fragment outputFragment;
+    private TextInputEditText inputEditText;
+    private Button buttonSubmit;
+    private RadioButton yesRadioButton;
+    private RadioButton noRadioButton;
+
     public ContentFragment() {
+        setRetainInstance(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_content, container, false);
-        Button button = view.findViewById(R.id.button);
-        button.setOnClickListener(view1 -> {
-            TextInputEditText inputEditText = view.findViewById(R.id.textInputEditText);
+
+        inputEditText = view.findViewById(R.id.textInputEditText);
+        buttonSubmit = view.findViewById(R.id.button);
+        yesRadioButton = view.findViewById(R.id.radio_yes);
+        noRadioButton = view.findViewById(R.id.radio_no);
+
+        buttonSubmit.setOnClickListener(view1 -> {
+
             String message = (inputEditText.getText() == null) ? null : inputEditText.getText().toString();
 
-            outputFragment = OutputFragment.newInstance(getOutputMessage(message));
+            message = String.format("Question: %s \nAnswer: %s", message,
+                    getResponseFromCheckBox(yesRadioButton.isChecked(), noRadioButton.isChecked()));
 
-            assert getFragmentManager() != null;
-            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment, outputFragment, null);
-            transaction.commit();
+            OutputFragment outputFragment = OutputFragment.newInstance(getOutputMessage(message));
+
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, outputFragment)
+                    .commit();
         });
         return view;
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save necessary data into the outState bundle
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        // Restore the fragment's state from the savedInstanceState bundle
+    }
+
+
     private String getOutputMessage(String message) {
-        return (message == null) ? "Hello from OutputFragment!" : message;
+        return (message == null) ? "No question provided" : message;
+    }
+
+    private String getResponseFromCheckBox(boolean answerIsYes, boolean answerIsNo) {
+        if (!answerIsYes && !answerIsNo) {
+            return "No Answer";
+        }
+        if (answerIsYes) {
+            return "Yes";
+        }
+        return "No";
     }
 
 }
